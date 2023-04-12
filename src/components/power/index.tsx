8bedde6754c437837ef8ca995170b7966af4ba94
kenny <kenny.caldieraro@gmail.com>
2023-04-12 21:30:26 +0200

    merge component

--- src/components/power/index.tsx
+++ src/components/power/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import {View, ViewStyle, StyleSheet} from 'react-native';
 
+import {Rotate} from '../index';
 interface LEDBarGraphProps {
-  value: number;
+  value: boolean;
   maxValue: number;
   numLEDs: number;
   ledColor?: string;
@@ -20,43 +21,68 @@ const LEDBarGraph: React.FC<LEDBarGraphProps> = ({
   ledSize = 15,
   spacing = -5,
 }) => {
-  const barFillPercentage = (value / maxValue) * 100;
-  const numEmptyLEDs = numLEDs - Math.floor((value / maxValue) * numLEDs);
-  const angleIncrement = 270 / (numLEDs - 1); // Angle entre chaque LED
-  const arcRadius = (ledSize + spacing) * (numLEDs - 1); // Rayon de l'arc de cercle
+  const [potentiometerValue, setPotentiometerValue] = React.useState(5);
+
+  React.useEffect(() => {
+    if (value) {
+      setPotentiometerValue(5);
+    } else {
+      setPotentiometerValue(0);
+    }
+  }, [value]);
+
+  const barFillPercentage = (potentiometerValue / maxValue) * 100;
+  const numEmptyLEDs =
+    numLEDs - Math.floor((potentiometerValue / maxValue) * numLEDs);
+  const angleIncrement = 270 / (numLEDs - 1);
+  const arcRadius = (ledSize + spacing) * (numLEDs - 1);
+
+  // callback potentiometer
+  const handlePotentiometerValueChange = (value: any) => {
+    setPotentiometerValue(value);
+  };
 
   return (
     <View
-      style={[
-        styles.container,
-        {
-          width: arcRadius + ledSize,
-          height: arcRadius + ledSize,
-        },
-      ]}>
-      {Array.from({length: numLEDs}, (_, index) => {
-        const isFilled = index >= numEmptyLEDs;
-        const rotationAngle = index * angleIncrement - 135; // Angle de rotation pour chaque LED
-        const ledStyle = {
-          width: ledSize,
-          height: ledSize,
-          borderRadius: ledSize / 2,
-          backgroundColor: isFilled ? ledColor : backgroundColor,
-          position: 'absolute',
-          top: arcRadius * Math.sin((rotationAngle * Math.PI) / 180), // Calcul de la position top en fonction de l'angle
-          left: arcRadius * Math.cos((rotationAngle * Math.PI) / 180), // Calcul de la position left en fonction de l'angle
-        };
-        return <View style={[styles.led, ledStyle]} key={index} />;
-      })}
+      style={{
+        width: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <View
+        style={[
+          styles.container,
+          {
+            width: arcRadius + ledSize,
+            height: arcRadius + ledSize,
+          },
+        ]}>
+        {Array.from({length: numLEDs}, (_, index) => {
+          const isFilled = index >= numEmptyLEDs;
+          const rotationAngle = index * angleIncrement - 135; // Angle de rotation pour chaque LED
+          const ledStyle = {
+            width: ledSize,
+            height: ledSize,
+            borderRadius: ledSize / 2,
+            backgroundColor: isFilled ? ledColor : backgroundColor,
+            position: 'absolute',
+            top: arcRadius * Math.sin((rotationAngle * Math.PI) / 180), // Calcul de la position top en fonction de l'angle
+            left: arcRadius * Math.cos((rotationAngle * Math.PI) / 180), // Calcul de la position left en fonction de l'angle
+          };
+          return <View style={[styles.led, ledStyle]} key={index} />;
+        })}
+      </View>
+      {/* <View style={{}}> */}
+      <Rotate onValueChange={handlePotentiometerValueChange} />
+      {/* </View> */}
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    justifyContent: 'center',
-    alignItems: 'center',
-    transform: [{rotate: '270deg'}],
+    transform: [{rotate: '270deg'}, {translateY: 50}, {translateX: 40}],
+    position: 'absolute',
   },
   led: {},
 });
